9b44bcff5711a62aba8d04c0ed25a33270fcfe9d
Tim Baumann <tim@timbaumann.info>
2012-04-22 14:31:18 +0200

    update javascript-operational-transformation; make name input unobtrusive; buttons for undo, redo and markdown specific buttons

--- bin/server.js
+++ bin/server.js
@@ -17,8 +17,8 @@ var io = socketIO.listen(app);
 
 // source: http://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
 io.configure('production', function () {
-  io.set('transports', ['xhr-polling']); 
-  io.set('polling duration', 10); 
+  io.set('transports', ['xhr-polling']);
+  io.set('polling duration', 10);
 });
 
 var str = "# This is a Markdown heading\n\n"
@@ -28,7 +28,18 @@ var str = "# This is a Markdown heading\n\n"
         + "Lorem *ipsum* dolor **sit** amet.\n\n"
         + "    $ touch test.txt";
 var server = new ot.CodeMirrorServer(str, io.sockets, [], function (socket, cb) {
-    socket.once('auth', function (auth) { cb(auth.name); });
+  cb(!!socket.mayEdit);
+});
+io.sockets.on('connection', function (socket) {
+  socket.on('login', function (obj) {
+    if (typeof obj.name !== 'string') {
+      console.error('obj.name is not a string');
+      return;
+    }
+    socket.mayEdit = true;
+    server.setName(socket, obj.name);
+    socket.emit('logged_in', {});
+  });
 });
 
 var port = process.env.PORT || 3000;
--- public/demo/client.js
+++ public/demo/client.js
@@ -17,23 +17,120 @@
     }, 800);
   })();*/
 
-  var name = window.prompt("Name");
+  var disabledRegex = /(^|\s+)disabled($|\s+)/;
 
-  socket.emit('auth', { name: name });
+  function enable (el) {
+    el.className = el.className.replace(disabledRegex, ' ');
+  }
 
-  socket.once('doc', function (obj) {
-    var str = obj.str, revision = obj.revision, users = obj.users;
+  function disable (el) {
+    if (!disabledRegex.test(el.className)) {
+      console.log(el.className);
+      el.className += ' disabled';
+    }
+  }
 
-    var editorWrapper = document.getElementById('editor-wrapper');
-    var cm = window.cm = CodeMirror(editorWrapper, {
-      lineNumbers: true,
-      lineWrapping: true,
-      mode: 'markdown',
-      value: str
+  function preventDefault (e) {
+    if (e.preventDefault) { e.preventDefault(); }
+  }
+
+  function stopPropagation (e) {
+    if (e.stopPropagation) { e.stopPropagation(); }
+  }
+
+  function stopEvent (e) {
+    preventDefault(e);
+    stopPropagation(e);
+  }
+
+  function removeElement (el) {
+    el.parentNode.removeChild(el);
+  }
+
+  function beginsWith (a, b) { return a.slice(0, b.length) === b; }
+  function endsWith (a, b) { return a.slice(a.length - b.length, a.length) === b; }
+
+  function wrap (chars) {
+    cm.operation(function () {
+      if (cm.somethingSelected()) {
+        var selection = cm.getSelection();
+        if (beginsWith(selection, chars) && endsWith(selection, chars)) {
+          cm.replaceSelection(selection.slice(chars.length, selection.length - chars.length));
+        } else {
+          cm.replaceSelection(chars + selection + chars);
+        }
+      } else {
+        var index = cm.indexFromPos(cm.getCursor());
+        cm.replaceSelection(chars + chars);
+        cm.setCursor(cm.posFromIndex(index + 2));
+      }
     });
+  }
 
-    var userListWrapper = document.getElementById('userlist-wrapper');
-    var cmClient = window.cmClient = new CodeMirrorClient(revision, cm, socket, name, users);
-    userListWrapper.appendChild(cmClient.userListEl);
+  function bold ()   { wrap('**'); }
+  function italic () { wrap('*'); }
+  function code ()   { wrap('`'); }
+
+  var editorWrapper = document.getElementById('editor-wrapper');
+  var cm = window.cm = CodeMirror(editorWrapper, {
+    lineNumbers: true,
+    lineWrapping: true,
+    mode: 'markdown',
+    onChange: function () {
+      if (!cmClient) { return; }
+      function enableConditionally (stack, el) {
+        if (stack.length === 0) { disable(el); }
+        else                    { enable(el); }
+      }
+      enableConditionally(cmClient.undoStack, undoBtn);
+      enableConditionally(cmClient.redoStack, redoBtn);
+    }
   });
+
+  var undoBtn = document.getElementById('undo-btn');
+  undoBtn.onclick = function (e) { cm.undo(); stopEvent(e); };
+  var redoBtn = document.getElementById('redo-btn');
+  redoBtn.onclick = function (e) { cm.redo(); stopEvent(e); };
+
+  var boldBtn = document.getElementById('bold-btn');
+  boldBtn.onclick = function (e) { bold(); stopEvent(e); };
+  disable(boldBtn);
+  var italicBtn = document.getElementById('italic-btn');
+  italicBtn.onclick = function (e) { italic(); stopEvent(e); };
+  disable(italicBtn);
+  var codeBtn = document.getElementById('code-btn');
+  disable(codeBtn);
+  codeBtn.onclick = function (e) { code(); stopEvent(e); };
+
+  var loginForm = document.getElementById('login-form');
+  loginForm.onsubmit = function (e) {
+    preventDefault(e);
+    var username = document.getElementById('username').value;
+    socket
+      .emit('login', { name: username })
+      .on('logged_in', function () {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(username + " (that's you!)"));
+        cmClient.clientListEl.appendChild(li);
+
+        enable(boldBtn);
+        enable(italicBtn);
+        enable(codeBtn);
+
+        removeElement(overlay);
+        removeElement(loginForm);
+      });
+  };
+
+  var overlay = document.createElement('div');
+  overlay.id = 'overlay';
+  overlay.onclick = stopPropagation;
+  overlay.onmousedown = stopPropagation;
+  overlay.onmouseup = stopPropagation;
+  var cmWrapper = cm.getScrollerElement();
+  cmWrapper.appendChild(overlay);
+
+  var userListWrapper = document.getElementById('userlist-wrapper');
+  var cmClient = window.cmClient = new CodeMirrorClient(socket, cm);
+  userListWrapper.appendChild(cmClient.clientListEl);
 })();
